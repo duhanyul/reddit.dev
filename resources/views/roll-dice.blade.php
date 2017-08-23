2baf44d92b5d308de6475d41811a4cb4a339ea0d
Daniel Rojo-Gutierrez <danielrojo-gutierrez@Daniels-MacBook-Pro.local>
2017-08-23 16:24:18 -0500

    refactored to use blade shorthand syntax for php

--- resources/views/roll-dice.blade.php
+++ resources/views/roll-dice.blade.php
@@ -1,17 +1,15 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Rolls Dice</title>
-    </head>
-    <body>
+@extends('layouts.master')
+@section('title')
+    <title>Roll the Dice</title>
+@endsection
+
+@section('content')
         <h1>Hey You rolled some die</h1>
         <h2>lets see what you guessed</h2>
-        <h3>Looks like a <?=$guess?></h3>
-        <?php if ($guess == $number): ?>
+        <h3>Looks like a {{ $guess }}</h3>
+        @if($guess == $number)
             <h4>You rolled the lucky magic number</h4>
-            <?php else: ?>
-                <h4>No dice Friendo</h4>
-        <?php endif; ?>
-    </body>
-</html>
+        @else
+            <h4>No dice Friendo</h4>
+        @endif
+@endsection
