bf220584f1a80bb92227da1428a3be5fd6807743
Daniel Rojo-Gutierrez <danielrojo-gutierrez@Daniels-MacBook-Pro.local>
2017-08-28 16:25:13 -0500

    added pagination button and date formating

--- resources/views/posts/all.blade.php
+++ resources/views/posts/all.blade.php
@@ -9,6 +9,8 @@
         <h1>{{$post->title}}</h1>
         <h2>{{$post->content}}</h2>
         <h3>{{$post->url}}</h3>
+        <h4>{{$post->created_at->setTimezone('America/Chicago')->format('l, F jS Y')}}</h4>
         <a href="{{action('PostsController@show',array($post->id))}}">Post {{$post->id}}</a>
     @endforeach
+    {!! $posts->render() !!}
 @endsection
