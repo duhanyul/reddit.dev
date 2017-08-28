25e54aeca013d0e10be87fedb202d263203dce3b
Daniel Rojo-Gutierrez <danielrojo-gutierrez@Daniels-MacBook-Pro.local>
2017-08-28 15:38:04 -0500

    added a rules static variable

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    public static $rules = [
+        'title' => 'required|min:2|max:200',
+        'url' => 'required'
+    ];
 }
