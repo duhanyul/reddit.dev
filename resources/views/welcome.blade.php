c82a51f1184021f7f32d15fdce18d95d2ff90c72
Daniel Rojo-Gutierrez <rojo@Daniels-MacBook-Pro.local>
2017-08-30 13:57:38 -0500

    made welcome blade use more templating

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,26 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
-
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    @extends('layouts.master')
 
         <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
             .content {
                 text-align: center;
                 display: inline-block;
             }
-
+            .contTitle{
+                display: table-cell;
+                vertical-align: middle;
+                text-align: center;
+            }
             .title {
                 font-size: 96px;
+
+                font-family: 'Lato';
             }
         </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
+        @section('content')
+
+            <div class="container contTitle">
+                <div class="content">
+                    <div class="title">Laravel 5</div>
+                </div>
             </div>
-        </div>
-    </body>
-</html>
+        @endsection
