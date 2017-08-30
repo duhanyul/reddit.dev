f0a9ec078c3e993475ffcad39945cc0bcf04d7fd
Daniel Rojo-Gutierrez <rojo@Daniels-MacBook-Pro.local>
2017-08-30 13:57:52 -0500

    added navbar to master blade

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,8 +4,31 @@
         <meta charset="utf-8">
         @yield('title')
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
     </head>
     <body>
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="/">WebSiteName</a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="/">Home</a></li>
+                    <li><a href="/posts">Posts</a></li>
+                    <li><a href=""></a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="/auth/register"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                    <li><a href="/auth/login"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                    <li><a href="/posts/create"><span>Make Post</span></a></li>
+                    <li>
+                        <form action=""></form>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
         <div class="container">
             @yield('content')
         </div>
