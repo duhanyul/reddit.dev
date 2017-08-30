cf369a9b7c06868736fd67f1889a1dc1ec317def
Daniel Rojo-Gutierrez <rojo@Daniels-MacBook-Pro.local>
2017-08-30 13:57:16 -0500

    change post seeder to generate 10000 posts as well randomly pick a user to create by

--- database/seeds/PostsSeeder.php
+++ database/seeds/PostsSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\User;
 use App\Models\Post;
 
 class PostsSeeder extends Seeder
@@ -13,12 +13,12 @@ class PostsSeeder extends Seeder
      */
     public function run()
     {
-        for ($i=1; $i <=10 ; $i++) {
+        for ($i=1; $i <=10000 ; $i++) {
             $post = new Post();
             $post->title = "Post title number: $i";
             $post->content = "Post content number: $i";
             $post->url = "Post URL number: $i";
-            $post->created_by = mt_rand(42,51);
+            $post->created_by =User::all()->random()->id;
             $post->save();
         }
     }
